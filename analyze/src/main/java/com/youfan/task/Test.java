1aa43af01bd0d648449ad1fe42695ef499c4acf1
iven <iven@ivens-iMac-Pro.local>
2020-08-11 07:47:12 +0800

    first commit

---
+++ analyze/src/main/java/com/youfan/task/Test.java
@@ -0,0 +1,25 @@
+package com.youfan.task;
+
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.utils.ParameterTool;
+
+public class Test {
+    public static void main(String[] args) {
+        final ParameterTool params = ParameterTool.fromArgs(args);
+        final ExecutionEnvironment environment = ExecutionEnvironment.getExecutionEnvironment();
+        environment.getConfig().setGlobalJobParameters(params);
+
+        DataSet<String> txt = environment.readTextFile("");
+        DataSet map = txt.flatMap(null);
+        DataSet reduce = map.groupBy("groupfield").reduce(null);
+        try {
+            environment.execute("text");
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
